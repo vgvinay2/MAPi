1327402882655f7e594a983095a9f4a6c6fcf812
vinay <vgvinay2@gmail.com>
2017-06-11 13:50:27 +0530

    first commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -8,7 +8,7 @@ class Doctor < ActiveRecord::Base
   def self.check_doctor_time(doctor_id,appointment_time)
     begin
       doctor = Doctor.find doctor_id
-      last_time = doctor.appointments.where("created_at >= ?", Date.current).last.created_at.strftime("%H:%M") if doctor.appointments.present?
+      last_time = doctor.appointments.where("created_at >= ?", Date.current).last.created_at if doctor.appointments.present?
       return appointment_time if last_time.nil?
       appointment_time = last_time + 20.minutes
       return appointment_time

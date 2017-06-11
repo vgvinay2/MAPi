a7dcaa56c2e73fdbe97f34a234f3c4cdee007a25
vinay <vgvinay2@gmail.com>
2017-06-11 13:48:38 +0530

    first commit

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -6,13 +6,15 @@ class Doctor < ActiveRecord::Base
     Doctor.all
   end
   def self.check_doctor_time(doctor_id,appointment_time)
-    doctor = Doctor.find_by_id doctor_id
-    last_time = doctor.appointments.where("created_at >= ?", Date.current).last.created_at.strftime("%H:%M") if doctor.appointments.present?
-    return appointment_time if last_time.nil?
-    # appointment_time ="appointment_time".to_datetime.strftime("%H:%M")
-    # get_appointment_time = Time.parse(appointment_time) - Time.parse(last_time) > 20 ? (Time.parse(appointment_time) - Time.parse(last_time) : appointment_time
-    byebug
-    return appointment_time = Time.parse(appointment_time) if (Time.parse(appointment_time) - Time.parse(last_time)) > 20
-    # Time.at(appointment_time).utc.strftime("%H:%M:%S")
+    begin
+      doctor = Doctor.find doctor_id
+      last_time = doctor.appointments.where("created_at >= ?", Date.current).last.created_at.strftime("%H:%M") if doctor.appointments.present?
+      return appointment_time if last_time.nil?
+      appointment_time = last_time + 20.minutes
+      return appointment_time
+    rescue Exception => e
+      logger.info "set appointments error  #{e.message}"
+    end
+    
   end
 end
